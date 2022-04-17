3ccb9b129773fa0b380ef334c1937d95620c04a4
Fuad <kamilovfuad@gmail.com>
2022-04-17 12:18:28 +0400

    Add textarea to home view, add some css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,24 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+    <div class="row">
+        <div class="col-6">
+            <div class="card mx-auto mt-4">
+                <div class="card-header">
+                    Post a new status
+                </div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <form action="" method="POST">
+                        <textarea name="textArea" id="textArea" cols="70"></textarea>
+                        <button class="btn btn-primary">Add Post</button>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="row">
+        
     </div>
 </div>
 @endsection
