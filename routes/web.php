3057adcc144ca47abb76f6153a2a1ae894e5c59f
Fuad <kamilovfuad@gmail.com>
2022-04-18 14:46:12 +0400

    change in routes leading to home page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/home');
+});
+
+Route::get("/posts", function() {
+    return view("/home");
 });
 
 Auth::routes();
